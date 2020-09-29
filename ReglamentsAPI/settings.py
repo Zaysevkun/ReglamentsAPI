2e7e5ec2fc2e521c8ecab1ceb3c5c7d2b816323b
zaysevkun <zaysev100@gmail.com>
2020-09-29 18:47:31 +1000

    added django_heroku

--- ReglamentsAPI/settings.py
+++ ReglamentsAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +122,5 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+django_heroku.settings(locals())
