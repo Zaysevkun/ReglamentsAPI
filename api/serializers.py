ebc2971a32a08f9dbb1ad89360748fb26c9d73e7
zaysevkun <zaysev100@gmail.com>
2020-10-03 19:20:29 +1000

    fix

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 
-from api.models import Department, Regulations, AdditionalUserInfo
+from api.models import Department, Regulations, AdditionalUserInfo, Revisions
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
