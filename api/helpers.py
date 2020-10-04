0358d36a7491f563ebf2811bfd7da85ca2db6b1b
Kirill <1234.kirill@list.ru>
2020-10-04 03:24:39 +0300

    Add helpers

---
+++ api/helpers.py
@@ -0,0 +1,11 @@
+from django.core.mail import EmailMessage
+
+
+def send_mail_to_department_users(departments):
+    emails = list(departments.filter(users__allow_send_info_emails=True).values_list(
+        'users__user__email', flat=True))
+    subject = f"Вы назначены утвердителем регламента"
+    body = ('Вы назначены утвердителем регламента. Тут должна быть ссылка на регламент,'
+            'Кирилл, не забудь поставить!!')
+    email = EmailMessage(subject, to=emails, body=body)
+    email.send()
