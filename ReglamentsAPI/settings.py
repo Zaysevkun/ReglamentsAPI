8c35406effc5e10595eb39adb93cd99d9a4d9c5f
Kirill <1234.kirill@list.ru>
2020-10-04 04:54:07 +0300

    Increase pagination number

--- ReglamentsAPI/settings.py
+++ ReglamentsAPI/settings.py
@@ -102,7 +102,7 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 100000,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
